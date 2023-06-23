925f4e8bfce6af35a2368fa8c2645263a3a1cf9a
giannurwana19 <giannurwana19@gmail.com>
2023-06-23 11:28:00 +0700

    menambahkan fungsi helper php dan javascript

---
+++ helper.js
@@ -0,0 +1,586 @@
+/**
+ * function untuk membuat format angka
+ *
+ * @param {number|string} number (angka)
+ * @param {string} decimals (jumlah desimal)
+ * @param {string} dec_point (simbol pemisah desimal)
+ * @param {string} thousands_sep (simbol pemisah ribuan)
+ * @returns {string}
+ */
+function numberFormat(
+  number,
+  decimals = 0,
+  dec_point = ',',
+  thousands_sep = '.'
+) {
+  number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = typeof thousands_sep === 'undefined' ? ',' : thousands_sep,
+    dec = typeof dec_point === 'undefined' ? '.' : dec_point,
+    s = '',
+    toFixedFix = function (n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
+// example:
+// console.log(numberFormat('950000.78', 2));
+
+/**
+ * function untuk membuat format tanggal indonesia
+ *
+ * @param {string|number} date
+ * @returns {string} date
+ */
+function formatTanggal(date) {
+  return new Date(date).toLocaleString('id-ID', {
+    // weekday: 'long', // long, short, narrow
+    day: 'numeric', // numeric, 2-digit
+    year: 'numeric', // numeric, 2-digit
+    month: 'long', // numeric, 2-digit, long, short, narrow
+    // hour: 'numeric', // numeric, 2-digit
+    // minute: 'numeric', // numeric, 2-digit
+    // second: 'numeric', // numeric, 2-digit
+  });
+}
+
+// example:
+// console.log(formatTanggal('2022-07-14'));
+
+/**
+ *
+ * @param {number|string} number
+ * @param {Object} settings
+ * @returns {string}
+ */
+function angkaTerbilang(number = 0, settings = { decimal: '.' }) {
+  const units = [
+    '',
+    'ribu',
+    'juta',
+    'milyar',
+    'triliun',
+    'quadriliun',
+    'quintiliun',
+    'sextiliun',
+    'septiliun',
+    'oktiliun',
+    'noniliun',
+    'desiliun',
+    'undesiliun',
+    'duodesiliun',
+    'tredesiliun',
+    'quattuordesiliun',
+    'quindesiliun',
+    'sexdesiliun',
+    'septendesiliun',
+    'oktodesiliun',
+    'novemdesiliun',
+    'vigintiliun',
+  ];
+  const maxIndex = units.length - 1;
+  function digitToUnit(digit) {
+    const curIndex = digit / 3;
+    return curIndex <= maxIndex ? units[curIndex] : units[maxIndex];
+  }
+
+  const numbers = [
+    '',
+    'satu',
+    'dua',
+    'tiga',
+    'empat',
+    'lima',
+    'enam',
+    'tujuh',
+    'delapan',
+    'sembilan',
+  ];
+
+  function numberToText(index) {
+    return numbers[index] || '';
+  }
+
+  const terbilang = angka => {
+    const angkaLength = angka.length;
+    const angkaMaxIndex = angkaLength - 1;
+
+    // Angka Nol
+    if (angkaMaxIndex === 0 && Number(angka[0]) === 0) {
+      return 'nol';
+    }
+
+    let space = '';
+    let result = '';
+
+    let i = 0;
+    while (i !== angkaLength) {
+      const digitCount = angkaMaxIndex - i;
+      const modGroup = digitCount % 3; // [2,1,0]
+      const curAngka = Number(angka[i]);
+
+      if (
+        digitCount === 3 &&
+        curAngka === 1 &&
+        (i === 0 || (Number(angka[i - 2]) === 0 && Number(angka[i - 1]) === 0))
+      ) {
+        /* Angka Seribu */
+        result += `${space}seribu`;
+      } else {
+        if (curAngka !== 0) {
+          if (modGroup === 0) {
+            /* Angka Satuan Bukan Nol */
+            result += `${space}${numberToText(curAngka)}${
+              i === angkaMaxIndex ? '' : ' '
+            }${digitToUnit(digitCount)}`;
+          } else if (modGroup === 2) {
+            /* Angka Ratusan */
+            if (curAngka === 1) {
+              result += `${space}seratus`;
+            } else {
+              result += `${space}${numberToText(curAngka)} ratus`;
+            }
+          } else {
+            /* Angka Sepuluh dan Belasan */
+            if (curAngka === 1) {
+              i++; // Skip Next Angka
+              const nextAngka = Number(angka[i]);
+              if (nextAngka === 0) {
+                result += `${space}sepuluh`;
+                /* Proses Next Angka Sekarang */
+                if (
+                  digitCount !== 1 &&
+                  (Number(angka[i - 2]) !== 0 || Number(angka[i - 1]) !== 0)
+                ) {
+                  result += ` ${digitToUnit(digitCount - 1)}`;
+                }
+              } else {
+                if (nextAngka === 1) {
+                  result += `${space}sebelas`;
+                } else {
+                  result += `${space}${numberToText(nextAngka)} belas`;
+                }
+                /* Proses Next Angka Sekarang */
+                if (digitCount !== 1) {
+                  result += ` ${digitToUnit(digitCount - 1)}`;
+                }
+              }
+            } else {
+              /* Angka Puluhan */
+              result += `${space}${numberToText(curAngka)} puluh`;
+            }
+          }
+        } else {
+          /* Angka Satuan Nol */
+          if (
+            modGroup === 0 &&
+            (Number(angka[i - 2]) !== 0 || Number(angka[i - 1]) !== 0) &&
+            digitCount !== 0
+          ) {
+            result += ` ${digitToUnit(digitCount)}`;
+          }
+        }
+      }
+
+      if (i <= 1) {
+        space = ' ';
+      }
+      i++;
+    }
+
+    return result;
+  };
+
+  const terbilangSatuSatu = angka => {
+    return angka
+      .split('')
+      .map(angka => (angka == 0 ? 'nol' : numberToText(angka)))
+      .join(' ');
+  };
+
+  if (typeof number !== 'string') number = String(number);
+  if (number.indexOf(settings.decimal) > -1) {
+    /* Dengan Desimal */
+    number = number.split(settings.decimal);
+    return `${terbilang(number[0])} koma ${terbilangSatuSatu(number[1])}`
+      .toLowerCase()
+      .replace(/(?<= )[^\s]|^./g, a => a.toUpperCase());
+  } else {
+    /* Tanpa Desimal */
+    return `${terbilang(number)}`
+      .toLowerCase()
+      .replace(/(?<= )[^\s]|^./g, a => a.toUpperCase());
+  }
+}
+
+// example
+// console.log(angkaTerbilang(2150880));
+
+/**
+ *
+ * @param {Date} dateVal
+ * @param {string} formatVal
+ * @returns {string}
+ */
+function dateFormat(dateVal, formatVal) {
+  // Defining locale
+  Date.shortMonths = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'Mei',
+    'Jun',
+    'Jul',
+    'Agu',
+    'Sep',
+    'Okt',
+    'Nov',
+    'Des',
+  ];
+  Date.longMonths = [
+    'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember',
+  ];
+  Date.shortDays = ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'];
+  Date.longDays = [
+    'Minggu',
+    'Senin',
+    'Selasa',
+    'Rabu',
+    'Kamis',
+    'Jumat',
+    'Sabtu',
+  ];
+  // Defining patterns
+  var replaceChars = {
+    // Day
+    d: function () {
+      var d = this.getDate();
+      return (d < 10 ? '0' : '') + d;
+    },
+    D: function () {
+      return Date.shortDays[this.getDay()];
+    },
+    j: function () {
+      return this.getDate();
+    },
+    l: function () {
+      return Date.longDays[this.getDay()];
+    },
+    N: function () {
+      var N = this.getDay();
+      return N === 0 ? 7 : N;
+    },
+    S: function () {
+      var S = this.getDate();
+      return S % 10 === 1 && S !== 11
+        ? 'st'
+        : S % 10 === 2 && S !== 12
+        ? 'nd'
+        : S % 10 === 3 && S !== 13
+        ? 'rd'
+        : 'th';
+    },
+    w: function () {
+      return this.getDay();
+    },
+    z: function () {
+      var d = new Date(this.getFullYear(), 0, 1);
+      return Math.ceil((this - d) / 86400000);
+    },
+    // Week
+    W: function () {
+      var target = new Date(this.valueOf());
+      var dayNr = (this.getDay() + 6) % 7;
+      target.setDate(target.getDate() - dayNr + 3);
+      var firstThursday = target.valueOf();
+      target.setMonth(0, 1);
+      if (target.getDay() !== 4) {
+        target.setMonth(0, 1 + ((4 - target.getDay() + 7) % 7));
+      }
+      var retVal = 1 + Math.ceil((firstThursday - target) / 604800000);
+      return retVal < 10 ? '0' + retVal : retVal;
+    },
+    // Month
+    F: function () {
+      return Date.longMonths[this.getMonth()];
+    },
+    m: function () {
+      var m = this.getMonth();
+      return (m < 9 ? '0' : '') + (m + 1);
+    },
+    M: function () {
+      return Date.shortMonths[this.getMonth()];
+    },
+    n: function () {
+      return this.getMonth() + 1;
+    },
+    t: function () {
+      var year = this.getFullYear();
+      var nextMonth = this.getMonth() + 1;
+      if (nextMonth === 12) {
+        year = year++;
+        nextMonth = 0;
+      }
+      return new Date(year, nextMonth, 0).getDate();
+    },
+    // Year
+    L: function () {
+      var L = this.getFullYear();
+      return L % 400 === 0 || (L % 100 !== 0 && L % 4 === 0);
+    },
+    o: function () {
+      var d = new Date(this.valueOf());
+      d.setDate(d.getDate() - ((this.getDay() + 6) % 7) + 3);
+      return d.getFullYear();
+    },
+    Y: function () {
+      return this.getFullYear();
+    },
+    y: function () {
+      return ('' + this.getFullYear()).substr(2);
+    },
+    // Time
+    a: function () {
+      return this.getHours() < 12 ? 'am' : 'pm';
+    },
+    A: function () {
+      return this.getHours() < 12 ? 'AM' : 'PM';
+    },
+    B: function () {
+      return Math.floor(
+        ((((this.getUTCHours() + 1) % 24) +
+          this.getUTCMinutes() / 60 +
+          this.getUTCSeconds() / 3600) *
+          1000) /
+          24
+      );
+    },
+    g: function () {
+      return this.getHours() % 12 || 12;
+    },
+    G: function () {
+      return this.getHours();
+    },
+    h: function () {
+      var h = this.getHours();
+      return ((h % 12 || 12) < 10 ? '0' : '') + (h % 12 || 12);
+    },
+    H: function () {
+      var H = this.getHours();
+      return (H < 10 ? '0' : '') + H;
+    },
+    i: function () {
+      var i = this.getMinutes();
+      return (i < 10 ? '0' : '') + i;
+    },
+    s: function () {
+      var s = this.getSeconds();
+      return (s < 10 ? '0' : '') + s;
+    },
+    v: function () {
+      var v = this.getMilliseconds();
+      return (v < 10 ? '00' : v < 100 ? '0' : '') + v;
+    },
+    // Timezone
+    e: function () {
+      return Intl.DateTimeFormat().resolvedOptions().timeZone;
+    },
+    I: function () {
+      var DST = null;
+      for (var i = 0; i < 12; ++i) {
+        var d = new Date(this.getFullYear(), i, 1);
+        var offset = d.getTimezoneOffset();
+        if (DST === null) DST = offset;
+        else if (offset < DST) {
+          DST = offset;
+          break;
+        } else if (offset > DST) break;
+      }
+      return (this.getTimezoneOffset() === DST) | 0;
+    },
+    O: function () {
+      var O = this.getTimezoneOffset();
+      return (
+        (-O < 0 ? '-' : '+') +
+        (Math.abs(O / 60) < 10 ? '0' : '') +
+        Math.floor(Math.abs(O / 60)) +
+        (Math.abs(O % 60) === 0
+          ? '00'
+          : (Math.abs(O % 60) < 10 ? '0' : '') + Math.abs(O % 60))
+      );
+    },
+    P: function () {
+      var P = this.getTimezoneOffset();
+      return (
+        (-P < 0 ? '-' : '+') +
+        (Math.abs(P / 60) < 10 ? '0' : '') +
+        Math.floor(Math.abs(P / 60)) +
+        ':' +
+        (Math.abs(P % 60) === 0
+          ? '00'
+          : (Math.abs(P % 60) < 10 ? '0' : '') + Math.abs(P % 60))
+      );
+    },
+    T: function () {
+      var tz = this.toLocaleTimeString(navigator.language, {
+        timeZoneName: 'short',
+      }).split(' ');
+      return tz[tz.length - 1];
+    },
+    Z: function () {
+      return -this.getTimezoneOffset() * 60;
+    },
+    // Full Date/Time
+    c: function () {
+      return this.format('Y-m-d\\TH:i:sP');
+    },
+    r: function () {
+      return this.toString();
+    },
+    U: function () {
+      return Math.floor(this.getTime() / 1000);
+    },
+  };
+  // Simulates PHP's date function
+  var date = new Date(dateVal);
+  return formatVal.replace(/(\\?)(.)/g, function (_, esc, chr) {
+    return esc === '' && replaceChars[chr] ? replaceChars[chr].call(date) : chr;
+  });
+}
+
+// example:
+// console.log(dateFormat(new Date('2022-05-07'), 'd F Y'));
+
+/**
+ *
+ * @param {string} length
+ * @returns {string}
+ */
+function uniqueId(length = 10) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+}
+
+// example
+// console.log(uniqueId(6));
+
+/**
+ *
+ * @param {number} length jumlah karakter invoice terakhir
+ * @param {string} prefix awalan invoice
+ * @returns {string}
+ */
+function generateInvoice(length = 10, prefix = 'INV') {
+  let characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let charactersLength = characters.length;
+  let randomString = '';
+
+  for (let i = 0; i < length; i++) {
+    randomString += characters.charAt(
+      Math.floor(Math.random() * charactersLength)
+    );
+  }
+
+  let date = new Date().toLocaleDateString().replace(/[/]/g, '');
+  let invoiceNumber = prefix + date + randomString;
+
+  return invoiceNumber;
+}
+
+// example
+// console.log(generateInvoice(5, 'RSV'));
+
+/**
+ *
+ * @param {Date} datetime
+ * @param {boolean} full
+ * @returns
+ */
+function timeAgo(datetime, full = false) {
+  const now = new Date();
+  const then = new Date(datetime);
+  const diffInMs = now - then;
+  const diffInSeconds = Math.floor(diffInMs / 1000);
+
+  const intervals = {
+    tahun: 31536000,
+    bulan: 2592000,
+    minggu: 604800,
+    hari: 86400,
+    jam: 3600,
+    menit: 60,
+    detik: 1,
+  };
+
+  const diff = {};
+  let remainingSeconds = diffInSeconds;
+
+  for (const [intervalName, seconds] of Object.entries(intervals)) {
+    if (remainingSeconds >= seconds) {
+      diff[intervalName] = Math.floor(remainingSeconds / seconds);
+      remainingSeconds %= seconds;
+    }
+  }
+
+  const string = [];
+  for (const [intervalName, value] of Object.entries(diff)) {
+    string.push(`${value} ${intervalName}`);
+  }
+
+  if (!full) {
+    string.splice(1);
+  }
+
+  return string.length ? `${string.join(', ')} yang lalu` : 'baru saja';
+}
+
+// example
+// console.log(timeAgo(new Date('2023-04-23')));
+
+/**
+ * fungsi untuk membuat slug
+ *
+ * @param string text
+ * @returns string
+ */
+function slugify(text) {
+  return text
+    .replace(/[^a-zA-Z0-9\s]/g, '')
+    .replace(/\s+/g, '-')
+    .toLowerCase()
+    .replace(/-+/g, '-')
+    .replace(/^-|-$/g, '');
+}
+
+// example
+console.log(slugify('Halo selamat Datang Gian '));
---
+++ helper.php
@@ -0,0 +1,425 @@
+<?php
+
+/**
+ * fungsi untuk membatasi limit karakter
+ *
+ * @param [type] $content
+ * @param integer $length panjang kata
+ * @param string $more pemisah
+ * @return string
+ */
+function force_excerpt($content, $length = 40, $more = '...')
+{
+    $excerpt = strip_tags(trim($content));
+    $words   = str_word_count($excerpt, 2);
+    if (count($words) > $length) {
+        $words = array_slice($words, 0, $length, true);
+        end($words);
+        $position = key($words) + strlen(current($words));
+        $excerpt  = substr($excerpt, 0, $position) . $more;
+    }
+    return $excerpt;
+}
+
+/**
+ * fungsi untuk membuat random string
+ *
+ * @param	string	type of random string.  basic, alpha, alnum, numeric, nozero, unique, md5, encrypt and sha1
+ * @param	int	number of characters
+ * @return	string
+ */
+function random_string($type = 'alnum', $len = 8)
+{
+    switch ($type) {
+        case 'basic':
+            return mt_rand();
+        case 'alnum':
+        case 'numeric':
+        case 'nozero':
+        case 'alpha':
+            switch ($type) {
+                case 'alpha':
+                    $pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                    break;
+                case 'alnum':
+                    $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                    break;
+                case 'numeric':
+                    $pool = '0123456789';
+                    break;
+                case 'nozero':
+                    $pool = '123456789';
+                    break;
+            }
+            return substr(str_shuffle(str_repeat($pool, ceil($len / strlen($pool)))), 0, $len);
+        case 'md5':
+            return md5(uniqid(mt_rand()));
+        case 'sha1':
+            return sha1(uniqid(mt_rand(), TRUE));
+    }
+}
+
+/**
+ * sample function untuk get data url
+ *
+ * @param [type] $id_hash
+ * @param [type] $judul
+ * @param [type] $message
+ * @return void
+ */
+function send_notifikasi_pengunjung($id_hash, $judul, $message)
+{
+    $useragent = "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0";
+    $url       = 'http://localhost';
+    $curl      = curl_init();
+    $fields    = array(
+        'id_hash' => $id_hash,
+        'judul'   => $judul,
+        'isi'     => $message,
+    );
+    $postfields = http_build_query($fields);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_USERAGENT, $useragent);
+    curl_setopt($curl, CURLOPT_USERPWD, 'username' . ':' . 'password');
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($curl);  //Getting jSON result string
+    curl_close($curl);
+}
+
+/**
+ * fungsi untuk generate waktu saat ini
+ * contoh: 2022-03-28 08: 54: 00
+ *
+ * @return DateTime
+ */
+function current_timestamp()
+{
+    return date("Y-m-d H:i:s");
+}
+
+/**
+ * fungsi untuk mengembalikan response json
+ *
+ * @param array $response
+ * @param integer $status_code
+ * @param string $content_type
+ * @return string json
+ */
+function response_json($response = [], $status_code = 200)
+{
+    http_response_code($status_code);
+    header("Content-Type: application/json");
+    echo json_encode($response);
+}
+
+/**
+ * fungsi untuk format angka
+ *
+ * @param integer $angka
+ * @return integer
+ */
+function format_angka(int $angka = 0)
+{
+    return number_format($angka, 0, ',', '.');
+}
+
+/**
+ * fungsi untuk format waktu readable / mudah dibaca
+ * contoh: 1 hari yang lalu, baru saja, 1 tahun yang lalu
+ *
+ * @param string|DateTime $time
+ * @return string
+ */
+function time_ago($datetime, $full = false)
+{
+    $now  = new DateTime;
+    $then = new DateTime($datetime);
+    $diff = (array) $now->diff($then);
+
+    $diff['w']  = floor($diff['d'] / 7);
+    $diff['d'] -= $diff['w'] * 7;
+
+    $string = array(
+        'y' => 'tahun',
+        'm' => 'bulan',
+        'w' => 'minggu',
+        'd' => 'hari',
+        'h' => 'jam',
+        'i' => 'menit',
+        's' => 'detik',
+    );
+
+    foreach ($string as $k => &$v) {
+        if ($diff[$k]) {
+            $v = $diff[$k] . ' ' . $v . ($diff[$k] > 1 ? '' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) {
+        $string = array_slice($string, 0, 1);
+    }
+
+    return $string ? implode(', ', $string) . ' yang lalu' : 'baru saja';
+}
+
+/**
+ * fungsi untuk konversi romawi menjadi angka
+ * contoh: V = 5
+ *
+ * @param string $romawi
+ * @return string
+ */
+function romawi_to_number(string $romawi)
+{
+    $table  = ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+    $result = 0;
+    foreach ($table as $key => $value) {
+        while (strpos($romawi, $key) === 0) {
+            $result += $value;
+            $romawi  = substr($romawi, strlen($key));
+        }
+    }
+    return $result;
+}
+
+/**
+ * fungis untuk konversi angka menjadi romawi
+ * contoh: 5 = V
+ *
+ * @param integer $integer
+ * @return string
+ */
+function number_to_romawi(int $integer)
+{
+    $table  = ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+    $return = '';
+    while ($integer > 0) {
+        foreach ($table as $rom => $arb) {
+            if ($integer >= $arb) {
+                $integer -= $arb;
+                $return  .= $rom;
+                break;
+            }
+        }
+    }
+
+    return $return;
+}
+
+/**
+ * fungsi date bahasa indonesia
+ *
+ * @param string|DateTime  $format
+ * @param boolean $time
+ * @return string
+ */
+function waktu_indo($format, $time = false)
+{
+    $day    = ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'];
+    $days   = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+    $month  = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'];
+    $months = ['', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+
+    if (!is_a($time, 'DateTime')) {
+        if (is_int($time)) {
+            $time = new DateTime(date('Y-m-d H:i:s.u', $time));
+        } elseif (is_string($time)) {
+            try {
+                $time = new DateTime($time);
+            } catch (Exception $e) {
+                $time = new DateTime();
+            }
+        } else {
+            $time = new DateTime();
+        }
+    }
+
+    $ret = '';
+    for ($i = 0; $i < strlen($format); $i++) {
+        switch ($format[$i]) {
+            case 'D':
+                $ret .= $day[$time->format('w')];
+                break;
+            case 'l':
+                $ret .= $days[$time->format('w')];
+                break;
+            case 'M':
+                $ret .= $month[$time->format('n')];
+                break;
+            case 'F':
+                $ret .= $months[$time->format('n')];
+                break;
+            case '\\':
+                $ret .= $format[$i + 1];
+                $i++;
+                break;
+            default:
+                $ret .= $time->format($format[$i]);
+                break;
+        }
+    }
+
+    return $ret;
+}
+
+/**
+ * helper function curl php
+ *
+ * @param string $url
+ * @param string $method GET, POST, PUT, PATCH, DELETE
+ * @param array $data
+ * @param array $headers
+ * @return json
+ */
+function fetch_curl($url, $method = 'GET', $data = null, $headers = array())
+{
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0');
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    switch (strtoupper($method)) {
+        case 'POST':
+            curl_setopt($curl, CURLOPT_POST, true);
+            break;
+        case 'PUT':
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+            break;
+        case 'DELETE':
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            break;
+        default:
+            curl_setopt($curl, CURLOPT_HTTPGET, true);
+    }
+
+    if ($data) {
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+    }
+
+    if ($headers) {
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    }
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($curl);
+
+    if ($response === false) {
+        $error = curl_error($curl);
+        curl_close($curl);
+        return $error;
+    }
+
+    curl_close($curl);
+
+    return $response;
+}
+
+/**
+ * helper untuk format angka showrt
+ *
+ * @param [type] $n
+ * @param integer $precision
+ * @return void
+ */
+function number_format_short($n, $precision = 1)
+{
+    if ($n < 900) {
+        // 0 - 900
+        $n_format = number_format($n, $precision);
+        $suffix = '';
+    } else if ($n < 900000) {
+        // 0.9k-850k
+        $n_format = number_format($n / 1000, $precision);
+        $suffix = 'K';
+    } else if ($n < 900000000) {
+        // 0.9m-850m
+        $n_format = number_format($n / 1000000, $precision);
+        $suffix = 'M';
+    } else if ($n < 900000000000) {
+        // 0.9b-850b
+        $n_format = number_format($n / 1000000000, $precision);
+        $suffix = 'B';
+    } else {
+        // 0.9t+
+        $n_format = number_format($n / 1000000000000, $precision);
+        $suffix = 'T';
+    }
+
+    if ($precision > 0) {
+        $dotzero = '.' . str_repeat('0', $precision);
+        $n_format = str_replace($dotzero, '', $n_format);
+    }
+
+    return $n_format . $suffix;
+}
+
+/**
+ * fungsi untuk mendapatkan range date
+ * start: 01-02-2023 sampai: 04-02-2023
+ * 01-02-2023, 02-02-2023, 03-02-2023, 04-02-2023
+ *
+ * @param string|DateTime $start_date
+ * @param string|DateTime $end_date
+ * @return array
+ */
+function get_range_date($start_date, $end_date)
+{
+    $list_range_date = [];
+
+    $date_from = mktime(1, 0, 0, substr($start_date, 5, 2), substr($start_date, 8, 2), substr($start_date, 0, 4));
+    $date_to = mktime(1, 0, 0, substr($end_date, 5, 2), substr($end_date, 8, 2), substr($end_date, 0, 4));
+
+    if ($date_to >= $date_from) {
+        array_push($list_range_date, date('Y-m-d', $date_from)); // first entry
+        while ($date_from < $date_to) {
+            $date_from += 86400; // add 24 hours
+            array_push($list_range_date, date('Y-m-d', $date_from));
+        }
+    }
+    return $list_range_date;
+}
+
+/**
+ * fungsi untuk generate invoice sederhana
+ *
+ * @param integer $length panjang karater acak terakhir
+ * @param string $prefix awalan invoice
+ * @return string
+ */
+function generate_invoice($length = 5, $prefix = 'INV')
+{
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $characters_length = strlen($characters);
+    $random_string = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $random_string .= $characters[rand(0, $characters_length - 1)];
+    }
+
+    $date = date('dmY');
+    return $prefix . $date . $random_string;
+}
+
+/**
+ * fungsi untuk membuat slug
+ *
+ * @param string $text
+ * @return string
+ */
+function slugify($text)
+{
+    $string = preg_replace('/[^a-zA-Z0-9\s]/', '', $text);
+    $string = str_replace(' ', '-', $string);
+    $string = strtolower($string);
+    $string = preg_replace('/-+/', '-', $string);
+    $string = trim($string, '-');
+
+    return $string;
+}
