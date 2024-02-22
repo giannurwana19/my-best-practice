382f58d3df989e248dbd75b3c68fe49b98508a2b
giannurwana19 <giannurwana19@gmail.com>
2024-02-22 08:52:15 +0700

    membuat helper UUID V4 di di php dan js

--- helper.js
+++ helper.js
@@ -765,3 +765,20 @@ function generatePassword(length = 8) {
 
   return password;
 }
+
+/**
+ * generate UUID V4
+ * @returns {string} uuid v4
+ */
+function generateUUIDV4() {
+  const randomHex = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+    /[xy]/g,
+    function (c) {
+      const r = (Math.random() * 16) | 0;
+      const v = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    }
+  );
+
+  return randomHex;
+}
--- helper.php
+++ helper.php
@@ -551,3 +551,24 @@ function generatePassword($length = 8)
 
     return $password;
 }
+
+/**
+ * generate UUID V4
+ *
+ * @return string uuidv4
+ */
+function generateUUIDV4()
+{
+    if (function_exists('random_bytes')) {
+        $data = random_bytes(16);
+    } elseif (function_exists('openssl_random_pseudo_bytes')) {
+        $data = openssl_random_pseudo_bytes(16);
+    } else {
+        trigger_error('Random number generator not available', E_USER_ERROR);
+    }
+
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
